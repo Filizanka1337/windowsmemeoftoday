2e134571e5817862a3f79ebe9be049d63f696212
Barteeq123 <93984012+Barteeq123@users.noreply.github.com>
2023-10-22 23:29:13 +0200

    Added stylesheet file and modifed index.html and script.js file.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function chooseMeme()
-{
-    return "images/meme" + Math.floor(Math.random * 14) + ".png";
+function chooseMeme() {
+    return "memes/meme" + Math.floor(Math.random() * 15) + ".png";
 }
 
 document.getElementById("img").src = chooseMeme();
+
+document.getElementById("btn").onclick = function() {
+    document.getElementById("img").src = chooseMeme();
+}
